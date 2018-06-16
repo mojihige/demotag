6b6dc675b2b4a43c78ab43ab284033bd8d6d421a
mojihige <ayatakayuram2605@gmail.com>
2018-06-16 18:42:48 +0900

    rename video column title

--- app/views/videos/_video.json.jbuilder
+++ app/views/videos/_video.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! video, :id, :titile, :director, :studio, :created_at, :updated_at
+json.extract! video, :id, :title, :director, :studio, :created_at, :updated_at
 json.url video_url(video, format: :json)
--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -17,7 +17,7 @@ class VideosControllerTest < ActionDispatch::IntegrationTest
 
   test "should create video" do
     assert_difference('Video.count') do
-      post videos_url, params: { video: { director: @video.director, studio: @video.studio, titile: @video.titile } }
+      post videos_url, params: { video: { director: @video.director, studio: @video.studio, title: @video.title } }
     end
 
     assert_redirected_to video_url(Video.last)
@@ -34,7 +34,7 @@ class VideosControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update video" do
-    patch video_url(@video), params: { video: { director: @video.director, studio: @video.studio, titile: @video.titile } }
+    patch video_url(@video), params: { video: { director: @video.director, studio: @video.studio, title: @video.title } }
     assert_redirected_to video_url(@video)
   end
 
